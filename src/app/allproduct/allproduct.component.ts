b35ee4cc9f4814155ebc8fb730577f15c4c4b77b
Qingyuan Chen <Qingyuan.Chen2@georgebrown.ca>
2022-04-22 13:10:31 -0400

    Final Project

--- src/app/allproduct/allproduct.component.ts
+++ src/app/allproduct/allproduct.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../services/product.service';
-
+import { ReturnStatement } from '@angular/compiler';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute,Params,Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-allproduct',
   templateUrl: './allproduct.component.html',
   styleUrls: ['./allproduct.component.scss']
 })
-export class AllproductComponent implements OnInit {
+export class AllproductComponent implements OnInit, OnDestroy{
+  user!: {name: number };
+  paramsSubscription!: Subscription;
+  constructor(private route: ActivatedRoute, private router: Router,public productservice: ProductService) { }
 
-  constructor(public productservice: ProductService) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.user = {
+      name: this.route.snapshot.params['name']
+    }    
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params)=>{
+        this.user.name= params['name'];
+      }
+    )
   }
-
+  productlist(id: number){
+    const product = this.productservice.product.filter(
+      s=>{
+        return (s.Id) ===(id);
+      }
+    )
+    return product;
+  }
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe(); 
+  }
 }
